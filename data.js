a0715348206c12bdd3408d63388b797dfbb0003c
lacykaltgr <68708534+lacykaltgr@users.noreply.github.com>
2020-07-23 22:26:11 +0200

    Add files via upload

---
+++ data.js
@@ -0,0 +1,25 @@
+const difficulties = [
+	{ len: 6, oprs: [.5, 1] },
+	{ len: 10, oprs: [.5, 1] },
+	{ len: 10, oprs: [.3, .6] }
+];
+
+const countdowns = [
+	['ODD', 'OR', 'EVEN'],
+	['EVEN', 'OR', 'ODD'],
+	['THREE', 'TWO', 'ONE'],
+	['READY', 'SET', 'GO'],
+	['HERE', 'WE', 'GO'],
+	["LET'S", 'GET', 'STARTED'],
+	['MATH', 'IS', 'FUN'],
+	['HIGH', 'SPEED', 'MATH'],
+	['GOTTA', 'THINK', 'FAST'],
+];
+
+const tutorials = [
+	{ s: '00', t: 'There are 2 teams,\nOdd and Even.' },
+	{ s: '01', t: 'The result is either\nodd or even.' },
+	{ s: '10', t: 'If the result matches your team,\npress the \u25EF button.' },
+	{ s: '10', t: "If it doesn't match your team,\npress the \u2A09 button." },
+	{ s: '11', t: 'Win by gaining the specified\npoint lead on your opponent.' }
+];
