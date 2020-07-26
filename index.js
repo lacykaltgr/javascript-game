8f15f7ba84c85386275bae4542c57c1f7ca028db
lacykaltgr <68708534+lacykaltgr@users.noreply.github.com>
2020-07-26 18:04:34 +0200

    Update index.js

--- index.js
+++ index.js
@@ -193,6 +193,17 @@ document.onreadystatechange = function () {
 	}
 };
 
+//csak próbálgatás
+class Quest {
+    constructor(type, content, penalty, special=None){
+        this.type = type
+        this.content = content
+        this.penalty = penalty
+        this.special = special
+    }
+}
+
+
 
 
 
