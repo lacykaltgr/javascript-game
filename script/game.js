7d5e7034726122b2592a520c7a5b07be3f7a36bf
lacykaltgr <68708534+lacykaltgr@users.noreply.github.com>
2020-07-28 18:21:30 +0200

    Update game.js

--- script/game.js
+++ script/game.js
@@ -1,1 +1,8 @@
-
+class Quest {
+    constructor(type, content, penalty, special=None){
+        this.type = type
+        this.content = content
+        this.penalty = penalty
+        this.special = special
+    }
+}
