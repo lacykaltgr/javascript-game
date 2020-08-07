a162faf5de0d921a564cd492737cf50787ebe1e5
nagykn <nagykristofnandor@outlook.hu>
2020-08-07 15:34:35 +0200

    checkbox

--- js/setdificulty.js
+++ js/setdificulty.js
@@ -1,14 +1,27 @@
 var DIFICULTY = parseInt(localStorage.DIFICULTY || 0);
+var INFOS = {
+    0:"A könnyű fokozat .... addshoghdofhao aohfohofhoafh ohafohof ",
+    1:"A közepes fokozat .....gf souihg uhweoh foqh fiqf hoq foi",
+    2:"A nehéz fokozat ..... ief iew fie ofwoe ",
+    3:"A hárdkor fokozat ..... fsanhah ohaf oha foihas fdh disa"
+}
+
 
 const selectInput = document.querySelector("#dificulty");
 const options = document.querySelectorAll("#dificulty > option")
-
+let info = document.querySelector("#info");
 
 document.addEventListener('DOMContentLoaded', (e) => {
     options[DIFICULTY].setAttribute("selected","selected")
+    showInfo();
 });
 
 function setDificulty(element) {
     DIFICULTY = document.querySelector("#dificulty").value
-    localStorage.DIFICULTY = DIFICULTY;    
+    localStorage.DIFICULTY = DIFICULTY;
+    showInfo();
+}
+
+function showInfo() {
+    info.innerText = INFOS[DIFICULTY]
 }
--- js/test3.js
+++ js/test3.js
@@ -1,6 +1,17 @@
-card = document.querySelector(".card");
-
-card.addEventListener("click", (event)=>{
-    
-    card.classList.add("example");
+cards = document.querySelectorAll(".card");
+picked = document.querySelector("#picked");
+let j = 0;
+cards.forEach((card,index) => {
+    card.style.transform = `rotate(${j+40*index}deg)`;
+    card.style.left = `${j+40*index}px`
+    card.addEventListener("click", (event)=>{
+        card.style.left = "0"
+        
+        card.classList.add("example");
+        card.addEventListener("animationend", ()=>{
+            card.classList.remove("example");
+            card.style ="";
+            picked.appendChild(card);
+        });
+    }); 
 });
