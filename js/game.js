7bf1b5b4fdd2ed8d7d2b8ec8be87cb65e9826137
nagykn <nagykristofnandor@outlook.hu>
2020-08-13 12:57:16 +0200

    in game pick

--- js/game.js
+++ js/game.js
@@ -1,6 +1,70 @@
 // import {shuffle} from "./rand.js"
 
 
+
+let topCard = document.querySelector("#top-card");
+let deck = document.querySelector(".deck");
+let CardHistory = document.querySelector(".card-history")
+let CardOn = document.querySelector(".card-on")
+let round = document.querySelector("#round")
+let l = 0;
+
+function createCardNode(text) {
+    let li = document.createElement("li");
+    li.classList.add("event-scene");
+    let card = document.createElement("div");
+    card.classList.add("event-card")
+    let front = document.createElement("div");
+    front.classList.add("card__face");
+    front.classList.add("card__face--front");
+    back = document.createElement("div");
+    back.classList.add("card__face");
+    back.classList.add("card__face--back")
+    back.innerText = Math.round(Math.random()*100);
+
+    card.appendChild(front);
+    card.appendChild(back);
+    li.appendChild(card);
+
+    front.addEventListener("click", (event)=>{
+        li.style.zIndex = l;
+        round.innerText = l+1;
+        deck.insertBefore(createCardNode("hi"),li);
+        anime({
+            targets: card,
+                translateX:"10em",
+                rotateY:"180deg",
+                duration: 3000,
+                easing: 'spring(1, 30, 11, 0)',
+                complete: function(anim) {
+                    CardOn.innerHTML = "";
+                    CardHistory.innerHTML = "";
+                    CardHistory.appendChild(card.cloneNode(true));
+                    CardOn.appendChild(card.cloneNode(true))
+                    li.remove()
+                    l++;
+                    anime({
+                        targets: ".card-on > .event-card",
+                        scale: 1.4,
+                        duration:1500,
+                    })
+                }
+        })
+        
+        
+        // card.addEventListener("animationend", (event)=>{
+        //     CardHistory.innerHTML = "";
+        //     CardHistory.appendChild(card);
+        //     li.remove()
+        // });
+    });
+
+    return li;
+    
+}
+deck.appendChild(createCardNode("hi"));
+
+
 // function pickCard() {
 //     let hand = document.querySelector(".hand"); 
 //     let pick = document.querySelector("#pick");
